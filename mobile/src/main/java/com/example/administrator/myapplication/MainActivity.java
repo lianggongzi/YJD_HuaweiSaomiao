f92fa05179dc555b312225601dbc532d3b71a712
Feng <1057380759@qq.com>
2018-08-07 14:08:48 +0800

    删除多余的注释

--- mobile/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ mobile/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.KeyEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//随便大师傅告诉大哥说的符合规定符合公司大佛结果及欧普
 public class MainActivity extends BaseActivity {
 
     TextView PAKID_Tv;
